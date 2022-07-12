de82c4cfc411273770bf25186b4dae1f7b337f33
rebeccaaaaaaaaaaa <rebeccaignacio1996@gmail.com>
2022-07-11 23:37:19 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+import Calculator from './main/Calculator';
+
+export default function App() {
+  return (
+    <>
+      <h1> Calculadora</h1>
+      <Calculator />
+    </>
+  );
+}
